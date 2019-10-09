424239924827af7dfb2344109eaeac7cb011834f
VijayNandakumarkumar <vijay.nandakumar30@gmail.com>
2019-10-10 00:07:05 +0530

    Initialized webpack, babel.

---
+++ starter/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: ['babel-polyfill', './src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
+}
